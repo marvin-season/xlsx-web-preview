5439a7c61fb399d339dec84f0eb62e52f32007b2
marvin <sea-son@foxmail.com>
2024-11-13 14:38:38 +0800

    👌 IMPROVE: 表格虚拟化 渲染

--- src/SheetPreview.tsx
+++ src/SheetPreview.tsx
@@ -1,7 +1,6 @@
 
-import { FC, useMemo, useState } from "react";
+import { FC, ReactNode, useEffect, useMemo, useRef, useState } from "react";
 import "./index.css";
-
 
 type Sheet = {
     name: string;
@@ -12,31 +11,58 @@ const SheetPreview: FC<{
     sheet: Sheet;
 }> = ({ sheet }) => {
     // sheet.data is so large, so we need to render the table dynamically.
-
     const [visibleRange, setVisibleRange] = useState({
         offset: 0,
-        limit: 10,
-    })
+        limit: 30,
+    });
+
+    const lastScrollTopRef = useRef(-1);
 
     const dataRows = useMemo(() => {
-        return sheet.data.slice(visibleRange.offset, visibleRange.offset + visibleRange.limit)
+        return sheet.data.map((item, index) => ({ ...item, __v_id: index })).slice(visibleRange.offset, visibleRange.offset + visibleRange.limit)
     }, [sheet.data, visibleRange])
 
-    return <div style={{ height: '300px', overflowY: 'scroll' }}>
-        <table className="excel-table">
+    useEffect(() => {
+        console.log({ dataRows });
+    }, [dataRows])
+
+    return <div style={{ height: '300px', overflowY: 'scroll' }} onScroll={(e) => {
+        const { scrollTop, clientHeight, scrollHeight } = e.target as HTMLDivElement;
+        if (scrollTop > lastScrollTopRef.current) {
+            // 向下滚动
+            console.log('scroll to bottom', scrollTop, clientHeight, scrollHeight);
+            // 底部还剩一页时，加载更多数据
+            if (scrollHeight - (scrollTop + clientHeight) <= clientHeight && visibleRange.offset + visibleRange.limit < sheet.data.length) {
+                setVisibleRange(prev => ({
+                    offset: prev.offset + 10,
+                    limit: prev.limit,
+                }))
+            }
+        } else if (scrollTop < lastScrollTopRef.current) {
+            // 向上滚动
+            console.log('scroll to top', scrollTop, clientHeight, scrollHeight);
+            // 顶部还剩一页时，加载更多数据
+            if (scrollTop <= clientHeight && visibleRange.offset > 0) {
+                const offset = visibleRange.offset - 10;
+                setVisibleRange(prev => ({
+                    offset: offset < 0 ? 0 : offset,
+                    limit: prev.limit,
+                }))
+            }
+        }
+        lastScrollTopRef.current = scrollTop;
+    }}>
+        <table className="excel-table" >
             <tbody>
                 {
-                    dataRows.map((row, rowIndex) => {
+                    dataRows.map((row) => {
                         return (
-                            <tr key={rowIndex}>
-                                {row.map((cell, cellIndex) => (
-                                    <td
-                                        key={cellIndex}
-                                        className={"wrap-column"} // 限制第1列的宽度
-                                    >
-                                        {cell || ""}
-                                    </td>
-                                ))}
+                            <tr key={row.__v_id}>
+                                {
+                                    Object.entries(row).map(([key, value]) => (
+                                        key === '__v_id' ? null : <td key={key} className={"wrap-column"}>{value as ReactNode}</td>
+                                    ))
+                                }
                             </tr>
                         )
                     })
